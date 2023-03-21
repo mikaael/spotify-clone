aae310be984ccc9818679ece1a23445d5b3675b5
Victor Kauan <victorkauan.dev@gmail.com>
2023-03-21 12:49:10 -0300

    :bug: fix: Fix footer social media icons
    - Fix Footer social media icons;
    - Adjust CardPremiumPlans map.

--- src/components/PremiunPlans/CardPremiumPlans.jsx
+++ src/components/PremiunPlans/CardPremiumPlans.jsx
@@ -1,200 +1,196 @@
-import "./index.css";
+import './index.css';
 
 const plans = [
   {
-    type: "Individual",
-    listenWithoutAds: "Ouça músicas sem anúncios",
-    listenOffline: "Ouça em qualquer lugar — até quando estiver offline",
-    listenInOrder: "Ouça músicas na ordem que quiser",
-    makePrepaidPlan: "Faça um plano pré-pago ou uma assinatura",
+    type: 'Individual',
+    listenWithoutAds: 'Ouça músicas sem anúncios',
+    listenOffline: 'Ouça em qualquer lugar — até quando estiver offline',
+    listenInOrder: 'Ouça músicas na ordem que quiser',
+    makePrepaidPlan: 'Faça um plano pré-pago ou uma assinatura',
   },
   {
-    type: "Duo",
-    premiumAccounts: "2 contas Premium para pessoas que moram juntas",
+    type: 'Duo',
+    premiumAccounts: '2 contas Premium para pessoas que moram juntas',
     listenOfflineOrder:
-      "Música sem anúncios, no modo offline e na ordem que você quiser",
-    makePrepaidPlan: "Faça um plano pré-pago ou uma assinatura",
+      'Música sem anúncios, no modo offline e na ordem que você quiser',
+    makePrepaidPlan: 'Faça um plano pré-pago ou uma assinatura',
   },
   {
-    type: "Família",
+    type: 'Família',
     premiumAccounts:
-      "6 contas Premium para familiares que moram no mesmo endereço",
-    blockExplicitContentMusic: "Bloqueie músicas com conteúdo explícito",
+      '6 contas Premium para familiares que moram no mesmo endereço',
+    blockExplicitContentMusic: 'Bloqueie músicas com conteúdo explícito',
     listenOfflineOrder:
-      "Música sem anúncios, no modo offline e na ordem que você quiser",
+      'Música sem anúncios, no modo offline e na ordem que você quiser',
     spotifyKids:
-      "Spotify Kids: um app separado, feito especialmente para crianças",
-    makePrepaidPlan: "Faça um plano pré-pago ou uma assinatura",
+      'Spotify Kids: um app separado, feito especialmente para crianças',
+    makePrepaidPlan: 'Faça um plano pré-pago ou uma assinatura',
   },
   {
-    type: "Universitário",
+    type: 'Universitário',
     premiumAccounts:
-      "Desconto especial para estudantes universitários elegíveis",
-    listenWithoutAds: "Ouça músicas sem anúncios",
-    listenOffline: "Ouça em qualquer lugar — até quando estiver offline",
-    listenInOrder: "Ouça músicas na ordem que quiser",
+      'Desconto especial para estudantes universitários elegíveis',
+    listenWithoutAds: 'Ouça músicas sem anúncios',
+    listenOffline: 'Ouça em qualquer lugar — até quando estiver offline',
+    listenInOrder: 'Ouça músicas na ordem que quiser',
   },
 ];
 
 export function CardPremiumPlans({ planeType, price, accountNumber }) {
   return (
-    <div class="cards__card">
-      <header class="card__header">
-        <ul class="header__labels">
-          <li class="labels__label labels__label--full">
+    <div className='cards__card'>
+      <header className='card__header'>
+        <ul className='header__labels'>
+          <li className='labels__label labels__label--full'>
             1 mês grátis ao assinar
           </li>
-          <li class="labels__label labels__label--outline">
+          <li className='labels__label labels__label--outline'>
             Pagamento único disponível
           </li>
         </ul>
 
-        <div class="header__plan-info">
-          <h3 class="plan-info__title">{planeType}</h3>
-          <p class="plan-info__price">{price}</p>
-          <p class="plan-info__account-number">{accountNumber}</p>
+        <div className='header__plan-info'>
+          <h3 className='plan-info__title'>{planeType}</h3>
+          <p className='plan-info__price'>{price}</p>
+          <p className='plan-info__account-number'>{accountNumber}</p>
         </div>
       </header>
 
-      <hr class="card__divider" />
+      <hr className='card__divider' />
 
-      {plans.map((plan) => {
-        return (
-          <>
-            {plan.type === planeType ? (
-              <div class="card__body">
-                <ul class="body__offers">
-                  <li class="offers__offer">
-                    <img
-                      src="./assets/icons/check.svg"
-                      alt="Ícone de marcado"
-                      class="offer__check-icon"
-                    />
-                    <p class="offer__description">
-                      {plan.type === "Individual"
-                        ? plan.listenWithoutAds
-                        : plan.type === "Duo"
-                        ? plan.premiumAccounts
-                        : plan.type === "Família"
-                        ? plan.premiumAccounts
-                        : plan.type === "Universitário"
-                        ? plan.premiumAccounts
-                        : null}
-                    </p>
-                  </li>
-                  <li class="offers__offer">
-                    <img
-                      src="./assets/icons/check.svg"
-                      alt="Ícone de marcado"
-                      class="offer__check-icon"
-                    />
-                    <p class="offer__description">
-                      {plan.type === "Individual"
-                        ? plan.listenOffline
-                        : plan.type === "Duo"
-                        ? plan.listenOfflineOrder
-                        : plan.type === "Família"
-                        ? plan.blockExplicitContentMusic
-                        : plan.type === "Universitário"
-                        ? plan.listenWithoutAds
-                        : null}
-                    </p>
-                  </li>
-                  <li class="offers__offer">
-                    <img
-                      src="./assets/icons/check.svg"
-                      alt="Ícone de marcado"
-                      class="offer__check-icon"
-                    />
-                    <p class="offer__description">
-                      {plan.type === "Individual"
-                        ? plan.listenInOrder
-                        : plan.type === "Duo"
-                        ? plan.makePrepaidPlan
-                        : plan.type === "Família"
-                        ? plan.listenOfflineOrder
-                        : plan.type === "Universitário"
-                        ? plan.listenOffline
-                        : null}
-                    </p>
-                  </li>
-                  <li class="offers__offer">
-                    <img
-                      src="./assets/icons/check.svg"
-                      alt="Ícone de marcado"
-                      class="offer__check-icon"
-                      style={
-                        plan.type === "Duo"
-                          ? { display: "none" }
-                          : { display: "block" }
-                      }
-                    />
-                    <p class="offer__description">
-                      {plan.type === "Individual"
-                        ? plan.makePrepaidPlan
-                        : plan.type === "Duo"
-                        ? null
-                        : plan.type === "Família"
-                        ? plan.spotifyKids
-                        : plan.type === "Universitário"
-                        ? plan.listenInOrder
-                        : null}
-                    </p>
-                  </li>
-                  <li class="offers__offer">
-                    <img
-                      src="./assets/icons/check.svg"
-                      alt="Ícone de marcado"
-                      class="offer__check-icon"
-                      style={
-                        plan.type === "Individual" ||
-                        plan.type === "Duo" ||
-                        plan.type === "Universitário"
-                          ? { display: "none" }
-                          : { display: "block" }
-                      }
-                    />
-                    <p class="offer__description">
-                      {plan.type === "Individual"
-                        ? null
-                        : plan.type === "Duo"
-                        ? null
-                        : plan.type === "Família"
-                        ? plan.makePrepaidPlan
-                        : plan.type === "Universitário"
-                        ? null
-                        : null}
-                    </p>
-                  </li>
-                </ul>
+      {plans.map((plan, index) => {
+        return plan.type === planeType ? (
+          <div className='card__body' key={index}>
+            <ul className='body__offers'>
+              <li className='offers__offer'>
+                <img
+                  src='./assets/icons/check.svg'
+                  alt='Ícone de marcado'
+                  className='offer__check-icon'
+                />
+                <p className='offer__description'>
+                  {plan.type === 'Individual'
+                    ? plan.listenWithoutAds
+                    : plan.type === 'Duo'
+                    ? plan.premiumAccounts
+                    : plan.type === 'Família'
+                    ? plan.premiumAccounts
+                    : plan.type === 'Universitário'
+                    ? plan.premiumAccounts
+                    : null}
+                </p>
+              </li>
+              <li className='offers__offer'>
+                <img
+                  src='./assets/icons/check.svg'
+                  alt='Ícone de marcado'
+                  className='offer__check-icon'
+                />
+                <p className='offer__description'>
+                  {plan.type === 'Individual'
+                    ? plan.listenOffline
+                    : plan.type === 'Duo'
+                    ? plan.listenOfflineOrder
+                    : plan.type === 'Família'
+                    ? plan.blockExplicitContentMusic
+                    : plan.type === 'Universitário'
+                    ? plan.listenWithoutAds
+                    : null}
+                </p>
+              </li>
+              <li className='offers__offer'>
+                <img
+                  src='./assets/icons/check.svg'
+                  alt='Ícone de marcado'
+                  className='offer__check-icon'
+                />
+                <p className='offer__description'>
+                  {plan.type === 'Individual'
+                    ? plan.listenInOrder
+                    : plan.type === 'Duo'
+                    ? plan.makePrepaidPlan
+                    : plan.type === 'Família'
+                    ? plan.listenOfflineOrder
+                    : plan.type === 'Universitário'
+                    ? plan.listenOffline
+                    : null}
+                </p>
+              </li>
+              <li className='offers__offer'>
+                <img
+                  src='./assets/icons/check.svg'
+                  alt='Ícone de marcado'
+                  className='offer__check-icon'
+                  style={
+                    plan.type === 'Duo'
+                      ? { display: 'none' }
+                      : { display: 'block' }
+                  }
+                />
+                <p className='offer__description'>
+                  {plan.type === 'Individual'
+                    ? plan.makePrepaidPlan
+                    : plan.type === 'Duo'
+                    ? null
+                    : plan.type === 'Família'
+                    ? plan.spotifyKids
+                    : plan.type === 'Universitário'
+                    ? plan.listenInOrder
+                    : null}
+                </p>
+              </li>
+              <li className='offers__offer'>
+                <img
+                  src='./assets/icons/check.svg'
+                  alt='Ícone de marcado'
+                  className='offer__check-icon'
+                  style={
+                    plan.type === 'Individual' ||
+                    plan.type === 'Duo' ||
+                    plan.type === 'Universitário'
+                      ? { display: 'none' }
+                      : { display: 'block' }
+                  }
+                />
+                <p className='offer__description'>
+                  {plan.type === 'Individual'
+                    ? null
+                    : plan.type === 'Duo'
+                    ? null
+                    : plan.type === 'Família'
+                    ? plan.makePrepaidPlan
+                    : plan.type === 'Universitário'
+                    ? null
+                    : null}
+                </p>
+              </li>
+            </ul>
 
-                <a href="#" class="body__buy-button">
-                  COMECE AGORA
+            <a href='#' className='body__buy-button'>
+              COMECE AGORA
+            </a>
+
+            {plan.type != 'Universitário' ? (
+              <p className='body__terms-and-conditions'>
+                <a href='#' className='terms-and-conditions__link'>
+                  Sujeito a Termos e Condições
                 </a>
-
-                {plan.type != "Universitário" ? (
-                  <p class="body__terms-and-conditions">
-                    <a href="#" class="terms-and-conditions__link">
-                      Sujeito a Termos e Condições
-                    </a>
-                    . O mês grátis não está disponível para usuários que já
-                    usaram o Premium.
-                  </p>
-                ) : (
-                  <p class="body__terms-and-conditions university">
-                    Oferta disponível somente para estudantes de instituições de
-                    ensino superior credenciadas. O mês grátis não está
-                    disponível para usuários que já experimentaram o Premium.{" "}
-                    <a href="#" class="terms-and-conditions__link">
-                      Sujeito a Termos e Condições
-                    </a>{" "}
-                    da oferta do Spotify de desconto para universitários.
-                  </p>
-                )}
-              </div>
-            ) : null}
-          </>
-        );
+                . O mês grátis não está disponível para usuários que já usaram o
+                Premium.
+              </p>
+            ) : (
+              <p className='body__terms-and-conditions university'>
+                Oferta disponível somente para estudantes de instituições de
+                ensino superior credenciadas. O mês grátis não está disponível
+                para usuários que já experimentaram o Premium.{' '}
+                <a href='#' className='terms-and-conditions__link'>
+                  Sujeito a Termos e Condições
+                </a>{' '}
+                da oferta do Spotify de desconto para universitários.
+              </p>
+            )}
+          </div>
+        ) : null;
       })}
     </div>
   );
--- src/components/PremiunPlans/index.jsx
+++ src/components/PremiunPlans/index.jsx
@@ -4,59 +4,59 @@ import { CardPremiumPlans } from "./CardPremiumPlans";
 
 export function PremiunPlans() {
   return (
-    <section class="premium-plans">
-      <div class="premium-plans__wrapper">
-        <header class="wrapper__header">
-          <h2 class="header__title">Escolha seu plano Premium</h2>
-          <h3 class="header__subtitle">
+    <section className="premium-plans">
+      <div className="premium-plans__wrapper">
+        <header className="wrapper__header">
+          <h2 className="header__title">Escolha seu plano Premium</h2>
+          <h3 className="header__subtitle">
             Ouça sem limites no seu celular, alto-falante e em outros
             dispositivos.
           </h3>
 
-          <div class="header__payment-methods">
-            <ul class="payment-methods__bank-cards">
+          <div className="header__payment-methods">
+            <ul className="payment-methods__bank-cards">
               <li>
                 <img
-                  class="bank-cards__card-image"
+                  className="bank-cards__card-image"
                   src="./assets/icons/bank-cards/visa.svg"
                   alt="Cartão Visa"
                 />
               </li>
               <li>
                 <img
-                  class="bank-cards__card-image"
+                  className="bank-cards__card-image"
                   src="./assets/icons/bank-cards/mastercard.svg"
                   alt="Cartão Mastercard"
                 />
               </li>
               <li>
                 <img
-                  class="bank-cards__card-image"
+                  className="bank-cards__card-image"
                   src="./assets/icons/bank-cards/amex.svg"
                   alt="Cartão American Express"
                 />
               </li>
               <li>
                 <img
-                  class="bank-cards__card-image"
+                  className="bank-cards__card-image"
                   src="./assets/icons/bank-cards/elo.svg"
                   alt="Cartão Elo"
                 />
               </li>
               <li>
                 <img
-                  class="bank-cards__card-image"
+                  className="bank-cards__card-image"
                   src="./assets/icons/bank-cards/hipercard.svg"
                   alt="Cartão Hipercard"
                 />
               </li>
             </ul>
-            <span class="payment-methods__more-methods-message">
+            <span className="payment-methods__more-methods-message">
               + outros 11
             </span>
           </div>
         </header>
-        <ul class="wrapper__cards">
+        <ul className="wrapper__cards">
           <CardPremiumPlans
             planeType={"Individual"}
             price={"R$ 19,90/mês após o período da oferta"}
