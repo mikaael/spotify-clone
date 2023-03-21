4dbe3516230a137dbf38a3e75c81fdcc57e30011
paulacynthia <pcynthia013@gmail.com>
2023-03-20 23:35:05 -0300

    :fix: Imports and map

--- src/components/PremiunPlans/CardPremiumPlans.jsx
+++ src/components/PremiunPlans/CardPremiumPlans.jsx
@@ -1,3 +1,5 @@
+import "./index.css";
+
 const plans = [
   {
     type: "Individual",
@@ -34,7 +36,7 @@ const plans = [
   },
 ];
 
-export function CardPremiumPlans({planeType, price, accountNumber}) {
+export function CardPremiumPlans({ planeType, price, accountNumber }) {
   return (
     <div class="cards__card">
       <header class="card__header">
@@ -56,72 +58,144 @@ export function CardPremiumPlans({planeType, price, accountNumber}) {
 
       <hr class="card__divider" />
 
-      <div class="card__body">
-        <ul class="body__offers">
-          <li class="offers__offer">
-            <img
-              src="./assets/icons/check.svg"
-              alt="Ícone de marcado"
-              class="offer__check-icon"
-            />
-            <p class="offer__description">
-              6 contas Premium para familiares que moram no mesmo endereço
-            </p>
-          </li>
-          <li class="offers__offer">
-            <img
-              src="./assets/icons/check.svg"
-              alt="Ícone de marcado"
-              class="offer__check-icon"
-            />
-            <p class="offer__description">
-              Bloqueie músicas com conteúdo explícito
-            </p>
-          </li>
-          <li class="offers__offer">
-            <img
-              src="./assets/icons/check.svg"
-              alt="Ícone de marcado"
-              class="offer__check-icon"
-            />
-            <p class="offer__description">
-              Música sem anúncios, no modo offline e na ordem que você quiser
-            </p>
-          </li>
-          <li class="offers__offer">
-            <img
-              src="./assets/icons/check.svg"
-              alt="Ícone de marcado"
-              class="offer__check-icon"
-            />
-            <p class="offer__description">
-              Spotify Kids: um app separado, feito especialmente para crianças
-            </p>
-          </li>
-          <li class="offers__offer">
-            <img
-              src="./assets/icons/check.svg"
-              alt="Ícone de marcado"
-              class="offer__check-icon"
-            />
-            <p class="offer__description">
-              Faça um plano pré-pago ou uma assinatura
-            </p>
-          </li>
-        </ul>
+      {plans.map((plan) => {
+        return (
+          <>
+            {plan.type === planeType ? (
+              <div class="card__body">
+                <ul class="body__offers">
+                  <li class="offers__offer">
+                    <img
+                      src="./assets/icons/check.svg"
+                      alt="Ícone de marcado"
+                      class="offer__check-icon"
+                    />
+                    <p class="offer__description">
+                      {plan.type === "Individual"
+                        ? plan.listenWithoutAds
+                        : plan.type === "Duo"
+                        ? plan.premiumAccounts
+                        : plan.type === "Família"
+                        ? plan.premiumAccounts
+                        : plan.type === "Universitário"
+                        ? plan.premiumAccounts
+                        : null}
+                    </p>
+                  </li>
+                  <li class="offers__offer">
+                    <img
+                      src="./assets/icons/check.svg"
+                      alt="Ícone de marcado"
+                      class="offer__check-icon"
+                    />
+                    <p class="offer__description">
+                      {plan.type === "Individual"
+                        ? plan.listenOffline
+                        : plan.type === "Duo"
+                        ? plan.listenOfflineOrder
+                        : plan.type === "Família"
+                        ? plan.blockExplicitContentMusic
+                        : plan.type === "Universitário"
+                        ? plan.listenWithoutAds
+                        : null}
+                    </p>
+                  </li>
+                  <li class="offers__offer">
+                    <img
+                      src="./assets/icons/check.svg"
+                      alt="Ícone de marcado"
+                      class="offer__check-icon"
+                    />
+                    <p class="offer__description">
+                      {plan.type === "Individual"
+                        ? plan.listenInOrder
+                        : plan.type === "Duo"
+                        ? plan.makePrepaidPlan
+                        : plan.type === "Família"
+                        ? plan.listenOfflineOrder
+                        : plan.type === "Universitário"
+                        ? plan.listenOffline
+                        : null}
+                    </p>
+                  </li>
+                  <li class="offers__offer">
+                    <img
+                      src="./assets/icons/check.svg"
+                      alt="Ícone de marcado"
+                      class="offer__check-icon"
+                      style={
+                        plan.type === "Duo"
+                          ? { display: "none" }
+                          : { display: "block" }
+                      }
+                    />
+                    <p class="offer__description">
+                      {plan.type === "Individual"
+                        ? plan.makePrepaidPlan
+                        : plan.type === "Duo"
+                        ? null
+                        : plan.type === "Família"
+                        ? plan.spotifyKids
+                        : plan.type === "Universitário"
+                        ? plan.listenInOrder
+                        : null}
+                    </p>
+                  </li>
+                  <li class="offers__offer">
+                    <img
+                      src="./assets/icons/check.svg"
+                      alt="Ícone de marcado"
+                      class="offer__check-icon"
+                      style={
+                        plan.type === "Individual" ||
+                        plan.type === "Duo" ||
+                        plan.type === "Universitário"
+                          ? { display: "none" }
+                          : { display: "block" }
+                      }
+                    />
+                    <p class="offer__description">
+                      {plan.type === "Individual"
+                        ? null
+                        : plan.type === "Duo"
+                        ? null
+                        : plan.type === "Família"
+                        ? plan.makePrepaidPlan
+                        : plan.type === "Universitário"
+                        ? null
+                        : null}
+                    </p>
+                  </li>
+                </ul>
 
-        <a href="#" class="body__buy-button">
-          COMECE AGORA
-        </a>
+                <a href="#" class="body__buy-button">
+                  COMECE AGORA
+                </a>
 
-        <p class="body__terms-and-conditions">
-          <a href="#" class="terms-and-conditions__link">
-            Sujeito a Termos e Condições
-          </a>
-          . O mês grátis não está disponível para usuários que já usaram o
-          Premium.
-        </p>
-      </div>
+                {plan.type != "Universitário" ? (
+                  <p class="body__terms-and-conditions">
+                    <a href="#" class="terms-and-conditions__link">
+                      Sujeito a Termos e Condições
+                    </a>
+                    . O mês grátis não está disponível para usuários que já
+                    usaram o Premium.
+                  </p>
+                ) : (
+                  <p class="body__terms-and-conditions university">
+                    Oferta disponível somente para estudantes de instituições de
+                    ensino superior credenciadas. O mês grátis não está
+                    disponível para usuários que já experimentaram o Premium.{" "}
+                    <a href="#" class="terms-and-conditions__link">
+                      Sujeito a Termos e Condições
+                    </a>{" "}
+                    da oferta do Spotify de desconto para universitários.
+                  </p>
+                )}
+              </div>
+            ) : null}
+          </>
+        );
+      })}
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,10 +3,13 @@ import Banner from "../../components/Banner";
 import FeaturesPremium from "../../components/FeaturesPremium";
 import { PremiunPlans } from "../../components/PremiunPlans";
 import { Footer } from "../../components/Footer";
+import Header from "../../components/Header";
+
 function Home() {
   return (
     <div>
       {/* <div id="vidal-wrapper"> */}
+        <Header/>
         <Banner />
         <FeaturesPremium />
         <PremiunPlans />
