41428cb8c6e64579950089ace6d8dd27821acaad
paulacynthia <pcynthia013@gmail.com>
2023-04-10 09:59:22 -0300

    ✨ feat: Changing mock data

--- src/__mocks__/playlists.js
+++ src/__mocks__/playlists.js
@@ -1,17 +1,55 @@
+import teamWork from "../assets/playlists/banners/pussyInBoots.png";
+
 export const playlists = [
   {
-    title: "Peaceful Piano",
-    description: "Relax and indulge with beautiful piano pieces.",
-    cover: "https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6",
+    title: "teamwork",
+    description: "trabalho em equipe até nas músicas 🤝",
+    cover: teamWork,
     songs: [
       {
-        name: "Tjärnheden",
-        author: "Farsjön",
+        name: "Feeling Good",
+        author: "Nina Simone",
         cover:
-          "https://i.scdn.co/image/ab67616d0000485128ccaf8cb23d857cb9361ec4",
-        album: "Fjäderlätt",
-        addedAt: new Date("2023-04-03"),
-        durationInSeconds: 100,
+          "https://i.scdn.co/image/ab67616d00004851425e9a15a4a1b9fe123a1aa7",
+        album: "I Put A Spell On You",
+        addedAt: new Date("2023-04-09"),
+        durationInSeconds: 154,
+      },
+      {
+        name: "I Was Made For Lovin'You",
+        author: "KISS",
+        cover:
+          "https://i.scdn.co/image/ab67616d000048514384b6976cadaec272114022",
+        album: "Dysnasty",
+        addedAt: new Date("2023-04-09"),
+        durationInSeconds: 271,
+      },
+      {
+        name: "Vem Cá",
+        author: "Altamira, Pelé MilFlows",
+        cover:
+          "https://i.scdn.co/image/ab67616d00001e023b09db5c0356ad00d2be5b90",
+        album: "Vem Cá",
+        addedAt: new Date("2023-04-09"),
+        durationInSeconds: 166,
+      },
+      {
+        name: "THE DAY",
+        author: "PornoGraffitti",
+        cover:
+          "https://i.scdn.co/image/ab67616d00004851a069b037b39aa53b43ef3d70",
+        album: "Butterfly Effect",
+        addedAt: new Date("2023-04-09"),
+        durationInSeconds: 241,
+      },
+      {
+        name: "Never Been Better",
+        author: "half•alive, Orla Gartland",
+        cover:
+          "https://i.scdn.co/image/ab67616d00004851c693deb0a087336920b871e5",
+        album: "Conditions Of A Punk",
+        addedAt: new Date("2023-04-09"),
+        durationInSeconds: 188,
       },
     ],
   },
