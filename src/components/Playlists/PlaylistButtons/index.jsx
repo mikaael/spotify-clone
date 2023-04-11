a417948a08095308cc453406c90c1d14e4bfb6b0
Victor Kauan <victorkauan.dev@gmail.com>
2023-04-11 02:05:08 -0300

    :sparkles: feat: Add play/pause function to playlist button
    - Refactor play music system;
    - Add play/pause function to playlist main button.

--- src/components/Playlists/PlaylistButtons/index.jsx
+++ src/components/Playlists/PlaylistButtons/index.jsx
@@ -1,18 +1,31 @@
-import { PlayIcon } from "@heroicons/react/24/solid";
+import { PauseIcon, PlayIcon } from "@heroicons/react/24/solid";
 import { HeartIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import { usePause } from "../../../contexts/PauseContext";
 
 export function PlaylistButtons({ playlistName }) {
+  const { isPaused, setIsPaused } = usePause();
+
   return (
     <div className="flex items-center mx-8 my-6">
       <div className="text-black bg-spotify-green-light mr-8 p-3.5 rounded-full transition-all hover:scale-105 hover:brightness-105 hover:cursor-pointer">
-        <PlayIcon className="w-7" />
+        {!isPaused ? (
+          <PauseIcon
+            className="w-7 aspect-square"
+            onClick={() => setIsPaused(true)}
+          />
+        ) : (
+          <PlayIcon
+            className="w-7 aspect-square"
+            onClick={() => setIsPaused(false)}
+          />
+        )}
       </div>
       <HeartIcon
-        className="text-white/70 w-9 mr-6 transition-colors hover:text-white"
+        className="text-white/70 w-9 aspect-square mr-6 transition-colors hover:text-white"
         title="Salvar na Sua Biblioteca"
       />
       <EllipsisHorizontalIcon
-        className="text-white/60 w-9 transition-colors hover:text-white"
+        className="text-white/60 w-9 aspect-square transition-colors hover:text-white"
         title={`Mais opções para ${playlistName}`}
       />
     </div>
